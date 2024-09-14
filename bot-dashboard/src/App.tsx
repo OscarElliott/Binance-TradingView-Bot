4268648f414947adc3012715455e835b76310d17
OscarElliott <OscarAnthonyElliott@gmail.com>
2024-09-14 18:29:23 +0930

    Updated App.tsx to be able to use the sidebar for navigation

--- bot-dashboard/src/App.tsx
+++ bot-dashboard/src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TradingBotDashboard from './components/add-bots';
 import Sidebar from './components/sidebar';
+import ManageBots from './components/manage-bots'; // Import other pages as needed
 
 export default function App() {
   return (
-    <div className="flex app-body bg-gray-900 text-gray-100">
-      <div>
+    <Router>
+      <div className="flex app-body bg-gray-900 text-gray-100">
         <Sidebar />
+        <main className="flex-1 min-h-screen p-4">
+          <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>} />
+            <Route path="/add-bots" element={<TradingBotDashboard />} />
+            <Route path="/manage-bots" element={<ManageBots />} />
+            {/* Add more routes for other components */}
+          </Routes>
+        </main>
       </div>
-      <main className="flex-1  min-h-screen p-4">
-        <TradingBotDashboard />
-      </main>
-    </div>
+    </Router>
   );
 }
